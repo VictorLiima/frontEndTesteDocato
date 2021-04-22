64dc66005f818cc4869f667270e5dd8c6acba4fd
João Victor de Lima Martins <jvictor2012@outloo.com>
2021-04-21 23:35:10 -0300

    Login OK

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  public  apiURL = 'localhost:3000'
+  constructor(private http : HttpClient) { }
 
-  constructor() { }
+  // async login(email:string, senha:string){
+  //   console.log(email, senha);
+
+  //   return this.http.post<any>('http://'+this.apiURL+'/login').toPromise();
+  // }
+
+  public login(eMail: string, password: string)  {
+    //buscando url do servidor no localstorage
+
+       const autenticacao = {
+          "email": eMail,
+            "senha": password
+        }
+            return this.http.post('http://'+this.apiURL+'/login', autenticacao).pipe(
+              map((response:any)=>{
+                console.log(response);
+                if(response){
+                  localStorage.setItem('Token', response.token);
+                }
+              })
+            );
+   }
+
+  async logout(){
+    
+  }
 }
